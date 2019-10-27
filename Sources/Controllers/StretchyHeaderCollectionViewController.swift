2581102d11aa6d080196455eaa4bc0cb4a81ab5c
Marcus Vinícius <veras.marcusvinicius@gmail.com>
2019-10-27 12:58:42 -0300

    app-delegate-file-modified-&-added-the-main-controller-to-the-project

---
+++ Sources/Controllers/StretchyHeaderCollectionViewController.swift
@@ -0,0 +1,17 @@
+//
+//  StretchyHeaderCollectionViewController.swift
+//  LearnStretchyHeaderCollectionView
+//
+//  Created by Marcus Vinícius on 27/10/19.
+//  Copyright © 2019 Marcus Vinícius. All rights reserved.
+//
+
+import UIKit
+
+class StretchyHeaderCollectionViewController: UICollectionViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        collectionView.backgroundColor = .red
+    }
+}
--- Sources/Startup/AppDelegate.swift
+++ Sources/Startup/AppDelegate.swift
@@ -14,7 +14,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        let controller = ViewController()
+        let layout = UICollectionViewFlowLayout()
+        let controller = StretchyHeaderCollectionViewController(collectionViewLayout: layout)
         let navigationController = UINavigationController(rootViewController: controller)
         
         window = UIWindow(frame: UIScreen.main.bounds)
